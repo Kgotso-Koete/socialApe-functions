d9562aa8becc8df71d88d0af7a77fcc2dc82354c
“Kgotso-Koete” <kgkoete@gmail.com>
2019-10-21 16:47:28 +0200

    Add part 7: Refactoring & Organising

---
+++ functions/util/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apiKey: "AIzaSyAHYLF_jRirH-PzRIcAuPx_HNlaGHv1WKA",
+  authDomain: "socialape-69760.firebaseapp.com",
+  databaseURL: "https://socialape-69760.firebaseio.com",
+  projectId: "socialape-69760",
+  storageBucket: "socialape-69760.appspot.com",
+  messagingSenderId: "321782010983",
+  appId: "1:321782010983:web:196a4d033bb04adeadf0c2",
+  measurementId: "G-E3XMMQ4DDL"
+};
